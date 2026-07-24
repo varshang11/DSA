7790ff65bf712c911aef844730322a07e6525aeb
Varsha N G <varsha.ng2006@gmail.com>
2026-07-24 14:27:07 +0530

    Time: 3 ms (23.01%), Space: 23.9 MB (10.5%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(nums[i]==nums[j])
-                    count++;
-            }
-            if(count==1)
-                return nums[i];
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     int count=0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[i]==nums[j])
+        //             count++;
+        //     }
+        //     if(count==1)
+        //         return nums[i];
+        // }
+        // return -1;
+
+        unordered_map<int,int> freq;
+        for(int num:nums)
+            freq[num]++;
+        for(auto it:freq){
+            if(it.second==1)
+                return it.first;
         }
         return -1;
-        
 
         // int ans=0;
         // for(int num:nums){
