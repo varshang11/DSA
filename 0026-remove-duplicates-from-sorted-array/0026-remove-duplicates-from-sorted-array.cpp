70e807127796a69c42d592eaa219862b9a96b9ae
Varsha N G <varsha.ng2006@gmail.com>
2026-07-24 09:40:11 +0530

    Time: 0 ms (100%), Space: 22.7 MB (53.82%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -14,7 +14,8 @@ public:
         int i=1;
         for(int j=1;j<nums.size();j++){
             if(nums[j]!=nums[i-1]){
-                nums[i++]=nums[j];
+                nums[i]=nums[j];
+                i++;
             }
         }
         return i;
