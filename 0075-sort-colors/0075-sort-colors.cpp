67c01b5286e268ddd8de9f33dc032a4a4e9b381e
Varsha N G <varsha.ng2006@gmail.com>
2026-07-29 11:31:37 +0530

    Time: 0 ms (100%), Space: 11.5 MB (79.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -11,34 +11,35 @@ public:
         //     }
         // }
 
-        int n=nums.size();
-        int cnt0=0,cnt1=0,cnt2=0;
-        for(int num:nums){
-            if(num==0) cnt0++;
-            else if(num==1) cnt1++;
-            else cnt2++;
-        }
-        for(int i=0;i<cnt0;i++)
-            nums[i]=0;
-        for(int i=cnt0;i<cnt0+cnt1;i++)
-            nums[i]=1;
-        for(int i=cnt0+cnt1;i<n;i++)
-            nums[i]=2;
-        // int low=0;
-        // int mid=0;
-        // int high=nums.size()-1;
-        // while(mid<=high){
-        //     if(nums[mid]==0){
-        //         swap(nums[mid],nums[low]);
-        //         low++;
-        //         mid++;
-        //     }
-        //     else if(nums[mid]==1)
-        //         mid++;
-        //     else{
-        //         swap(nums[mid],nums[high]);
-        //         high--;
-        //     }
+        // int n=nums.size();
+        // int cnt0=0,cnt1=0,cnt2=0;
+        // for(int num:nums){
+        //     if(num==0) cnt0++;
+        //     else if(num==1) cnt1++;
+        //     else cnt2++;
         // }
+        // for(int i=0;i<cnt0;i++)
+        //     nums[i]=0;
+        // for(int i=cnt0;i<cnt0+cnt1;i++)
+        //     nums[i]=1;
+        // for(int i=cnt0+cnt1;i<n;i++)
+        //     nums[i]=2;
+
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+                mid++;
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
     }
 };
