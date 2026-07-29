64d0f1503529e408af268877a97cf8a5e490dd97
Varsha N G <varsha.ng2006@gmail.com>
2026-07-29 15:29:57 +0530

    Time: 5 ms (64.52%), Space: 128.3 MB (78.32%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -18,7 +18,7 @@ public:
 
         int pos=0;
         int neg=1;
-        vector<int> ans(nums.size());
+        vector<int> ans(nums.size(),0);
         for(int num:nums){
             if(num>0){
                 ans[pos]=num;
