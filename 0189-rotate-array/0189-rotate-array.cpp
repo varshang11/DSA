d33f26d68600c53927b440c070ddd3957234afe0
Varsha N G <varsha.ng2006@gmail.com>
2026-07-30 09:57:06 +0530

    Time: 0 ms (100%), Space: 29.5 MB (70.71%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // void rotate(vector<int>& nums, int k) {
+    void rotate(vector<int>& nums, int k) {
         // int n=nums.size();
         // int index=0;
         // int arr[n];
@@ -12,26 +12,26 @@ public:
         // for(int i=0;i<n;i++)
         //     nums[i]=arr[i];
 
-    //    int n=nums.size();
-    //    k=k%n;
-    //    reverse(nums.begin(),nums.begin()+(n-k));
-    //    reverse(nums.begin()+(n-k),nums.end());
-    //    reverse(nums.begin(),nums.end());
+       int n=nums.size();
+       k=k%n;
+       reverse(nums.begin(),nums.begin()+(n-k));
+       reverse(nums.begin()+(n-k),nums.end());
+       reverse(nums.begin(),nums.end());
+    }
+
+    // void reverseArray(vector<int>& nums, int start, int end) {
+    //     while (start < end) {
+    //         swap(nums[start], nums[end]);
+    //         start++;
+    //         end--;
+    //     }
     // }
+    // void rotate(vector<int>& nums, int k) {
+    //     int n = nums.size();
+    //     k = k % n;
 
-    void reverseArray(vector<int>& nums, int start, int end) {
-        while (start < end) {
-            swap(nums[start], nums[end]);
-            start++;
-            end--;
-        }
-    }
-    void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k % n;
-
-        reverseArray(nums, 0, n - k - 1);
-        reverseArray(nums, n - k, n - 1);
-        reverseArray(nums, 0, n - 1);
-    }
+    //     reverseArray(nums, 0, n - k - 1);
+    //     reverseArray(nums, n - k, n - 1);
+    //     reverseArray(nums, 0, n - 1);
+    // }
 };
