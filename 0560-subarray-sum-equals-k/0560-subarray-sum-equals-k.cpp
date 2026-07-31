e124967291e18d6c92858715931edb17c12e8a4a
Varsha N G <varsha.ng2006@gmail.com>
2026-07-31 12:19:56 +0530

    Time: 35 ms (86.66%), Space: 45.5 MB (56.23%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -13,17 +13,30 @@ public:
         //             count++;
         //     }
         // }
+        // return count;          //TLE
+
+        // int n=nums.size();
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     int sum=0;
+        //     for(int j=i;j<n;j++){
+        //         sum+=nums[j];
+        //         if(sum==k)
+        //             count++;
+        //     }
+        // }
         // return count;
 
-        int n=nums.size();
-        int count=0;
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==k)
-                    count++;
-            }
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        int prefSum=0,count=0;
+        for(int i=0;i<nums.size();i++){
+            prefSum+=nums[i];
+        
+            if(mp.find(prefSum-k)!=mp.end())
+                count+=mp[prefSum-k];
+            
+            mp[prefSum]++;
         }
         return count;
     }
