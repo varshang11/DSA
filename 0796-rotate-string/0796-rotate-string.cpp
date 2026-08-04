c55d47be2342040b98781e320231f617931f777e
Varsha N G <varsha.ng2006@gmail.com>
2026-08-04 11:11:31 +0530

    Time: 0 ms (100%), Space: 8 MB (88.58%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -15,9 +15,23 @@ public:
         // }
         // return false;
 
+        // if(s.size()!=goal.size())
+        //     return false;
+        // int n=s.size();
+        // for(int i =0;i<n;i++){
+        //     rotate(s.begin(),s.begin()+1,s.end());
+        //     if(s== goal){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        
+
         if(s.size()!=goal.size())
             return false;
         return (s+s).find(goal)!=string::npos;
+
+
 
     }
 };
