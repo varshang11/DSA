4905535e319b45f9364cbeed4ff02bc3c1fa7862
Varsha N G <varsha.ng2006@gmail.com>
2026-08-04 16:32:35 +0530

    Time: 115 ms (48.58%), Space: 8.9 MB (93.5%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n=s.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            int freq[26]={0};
+            for(int j=i;j<n;j++){
+                freq[s[j]-'a']++;
+
+                int maxi=0;
+                int mini=INT_MAX;
+                for(int k=0;k<26;k++){
+                    if(freq[k]>0){
+                        maxi=max(maxi,freq[k]);
+                        mini=min(mini,freq[k]);
+                    }
+                }
+                sum+=(maxi-mini);
+            }
+        }
+        return sum;
+    }
+};
