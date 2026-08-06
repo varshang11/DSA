ec72bae92b1f03dabfdabd71cd69d36e3bf7c347
Varsha N G <varsha.ng2006@gmail.com>
2026-08-06 10:14:04 +0530

    Time: 63 ms (20.11%), Space: 18.7 MB (39.64%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -11,21 +11,36 @@ public:
         // }
         // return false;        
 
+        // int rows=matrix.size();
+        // int cols=matrix[0].size();
+        // for(int i=0;i<rows;i++){
+        //     int low=0;
+        //     int high=cols-1;
+        //     while(low<=high){
+        //         int mid=low+(high-low)/2;
+        //         if(matrix[i][mid]==target)
+        //             return true;
+        //         else if(matrix[i][mid]<target)
+        //             low=mid+1;
+        //         else 
+        //             high=mid-1;
+        //     }
+        // }
+        // return false; 
+
+
         int rows=matrix.size();
-        int cols=matrix[0].size();
-        for(int i=0;i<rows;i++){
-            int low=0;
-            int high=cols-1;
-            while(low<=high){
-                int mid=low+(high-low)/2;
-                if(matrix[i][mid]==target)
-                    return true;
-                else if(matrix[i][mid]<target)
-                    low=mid+1;
-                else 
-                    high=mid-1;
-            }
+        int cols=matrix[0].size();        
+        int row=0;
+        int col=cols-1;
+        while(row<rows && col>=0){
+            if(matrix[row][col]==target)
+                return true;
+            else if(matrix[row][col]>target)
+                col--;
+            else 
+                row++;
         }
-        return false;                      
+        return false;            
     }
 };
