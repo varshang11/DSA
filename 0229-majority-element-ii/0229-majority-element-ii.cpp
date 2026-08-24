b11797dd626d14f5193fa4f5db8b8fe88ce2e2bc
Varsha N G <varsha.ng2006@gmail.com>
2026-08-25 00:05:04 +0530

    Time: 0 ms (100%), Space: 23.6 MB (93.78%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,6 +2,22 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
 
+        // vector<int> ans;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++) {
+        //     int count=0;
+        //     for(int j=0;j<n;j++) {
+        //         if(nums[i]==nums[j])
+        //             count++;
+        //     }
+        //     if(count>n/3) {
+        //         if(find(ans.begin(),ans.end(),nums[i])==ans.end())
+        //             ans.push_back(nums[i]);
+        //     }
+        // }
+        // return ans;
+
+
         // unordered_map<int,int> countElement;
         // for(int num:nums)
         //     countElement[num]++;
@@ -24,11 +40,11 @@ public:
                 count2++;
             else if(count1==0){
                 candidate1=num;
-                count1++;
+                count1=1;
             }
             else if(count2==0){
                 candidate2=num;
-                count2++;
+                count2=1;
             }
             else{
                 count1--;
