bc961f2dea85990ab0fb848e27c36fde2c3dad8e
Varsha N G <varsha.ng2006@gmail.com>
2026-08-25 00:05:52 +0530

    Time: 49 ms (48.74%), Space: 29.1 MB (46.34%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,10 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+
+        // int n=nums.size();
+        // set<vector<int>> st;
+        // for(int i=0;i<n;i++) {
+        //     for(int j=i+1;j<n;j++) {
+        //         for(int k=j+1;k<n;k++) {
+        //             if(nums[i]+nums[j]+nums[k]==0) {
+        //                 vector<int> temp={nums[i],nums[j],nums[k]};
+        //                 sort(temp.begin(),temp.end());
+        //                 st.insert(temp);
+        //             }
+        //         }
+        //     }
+        // }
+        // vector<vector<int>> ans(st.begin(), st.end());
+        // return ans;                                                 //TLE
+
+
+        // int n=nums.size();
+        // set<vector<int>> st;
+        // for(int i=0;i<n;i++){
+        //     unordered_set<int> hashset;
+        //     for(int j=i+1;j<n;j++){
+        //         int third=-(nums[i]+nums[j]);
+        //         if(hashset.find(third)!=hashset.end()){
+        //             vector<int> temp={nums[i],nums[j],third};
+        //             sort(temp.begin(),temp.end());
+        //             st.insert(temp);
+        //         }
+        //         hashset.insert(nums[j]);
+        //     }
+        // }
+        // vector<vector<int>> ans(st.begin(),st.end());
+        // return ans;
+
+
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         int n=nums.size();
-        for(int i=0;i<n-2;i++){
+        for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1])
                 continue;
             int left=i+1;
