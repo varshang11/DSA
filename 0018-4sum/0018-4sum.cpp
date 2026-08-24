dd78f252b2fabe1838a4b6e091b01d6257ac433f
Varsha N G <varsha.ng2006@gmail.com>
2026-08-25 00:06:01 +0530

    Time: 19 ms (80.98%), Space: 17.4 MB (56.86%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,13 +1,52 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+
+        // int n = nums.size();
+        // set<vector<int>> st;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i + 1; j < n; j++) {
+        //         for (int k = j + 1; k < n; k++) {
+        //             for (int l = k + 1; l < n; l++) {
+        //                 long long sum = (long long)nums[i]+ nums[j]+ nums[k]+ nums[l];
+        //                 if (sum == target) {
+        //                     vector<int> temp = {nums[i],nums[j],nums[k],nums[l]};
+        //                     sort(temp.begin(), temp.end());
+        //                     st.insert(temp);
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // return vector<vector<int>>(st.begin(), st.end());
+
+
+        // int n = nums.size();
+        // set<vector<int>> st;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i + 1; j < n; j++) {
+        //         unordered_set<int> hashset;
+        //         for (int k = j + 1; k < n; k++) {
+        //             long long fourth =target - (long long)nums[i]- nums[j] - nums[k];
+        //             if (hashset.find(fourth) != hashset.end()) {
+        //                 vector<int> temp = {nums[i],nums[j],nums[k],(int)fourth};
+        //                 sort(temp.begin(), temp.end());
+        //                 st.insert(temp);
+        //             }
+        //             hashset.insert(nums[k]);
+        //         }
+        //     }
+        // }
+        // return vector<vector<int>>(st.begin(), st.end());
+        
+        
         sort(nums.begin(),nums.end());
         int n=nums.size();
         vector<vector<int>> ans;
-        for(int i=0;i<n-3;i++){
+        for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1])
                 continue;
-            for(int j=i+1;j<n-2;j++){
+            for(int j=i+1;j<n;j++){
                 if(j>i+1 && nums[j]==nums[j-1])
                     continue;
                     int left=j+1;
